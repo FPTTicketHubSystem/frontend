e5fd2bc7f3bdbec64a76a573dc5ea0eeb3e27b0b
ppduc <ducppde150277@fpt.edu.vn>
2024-08-10 01:51:11 +0700

    update payment payos

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -54,7 +54,7 @@ export const ReturnPaymentUrl = async (data) => {
 export const CheckInputCoupon = async (id , discount) => {
   try {
       const response = await request({
-          method: 'post',
+          method: 'get',
           url: `payment/checkInputCoupon?eventId=${id}&coupon=${discount}`,
           headers: {
               'Content-Type': 'application/json',
@@ -80,5 +80,36 @@ export const CheckOrderId = async (orderId) => {
       return e;
   }
 };
+
+export const CancelOrderOfUser = async (userId) => {
+  try {
+      const response = await request({
+          method: 'get',
+          url: `/payment/CancelOrderOfUser?userId=${userId}`,
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      });
+      return response;
+  } catch (e) {
+      return e;
+  }
+};
+
+export const CheckOrderdOfUser = async (userId, eventId) => {
+  try {
+      const response = await request({
+          method: 'get',
+          url: `/payment/CheckOrderdOfUser?userId=${userId}&eventId=${eventId}`,
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      });
+      return response;
+  } catch (e) {
+      return e;
+  }
+};
+
 
 
